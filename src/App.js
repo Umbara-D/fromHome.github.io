cda80010ef01b4b7db85e930612b6c37bab6f34b
Umbara Diki Pratama <yanthieka66@gmail.com>
2020-11-12 11:55:24 +0800

    ..

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Index from "./views/Index.jsx";
 import BookPage from "./views/Browse/BookPage.jsx";
-import TentangKami from './views/Browse/TentangKami.jsx';
 
-import BookCard from "./views/Browse/BookCard.jsx";
-import TentangKami from "./views/Browse/TentangKami.jsx";
-import SearchResultList from './views/Search/SearchResultList.jsx';
-import Recommended from './views/Recommended/Recommended.jsx';
-import BookDetail from './views/Book/BookDetail/BookDetail.jsx';
-
-import BookCard from "./views/Browse/BookCard.jsx";
 import TentangKami from "./views/Browse/TentangKami.jsx";
 import SearchResultList from './views/Search/SearchResultList.jsx';
 import Recommended from './views/Recommended/Recommended.jsx';
@@ -24,9 +16,7 @@ function App() { //stateless component
     <>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Index}></Route>    
-          {/* <Route path="/book" exact component={BookPage}></Route>    */}
-          <Route path="/card" exact component={BookCard}></Route>
+          <Route path="/" exact component={Index}></Route>
           <Route path="/tentang" exact component={TentangKami}></Route> 
           <Route path="/search" exact component={SearchResultList}></Route>   
           <Route path="/recommended" exact component={Recommended}></Route>  
