2d4a48f373ae58ef3a764cb68b3f2df6a322cb00
Desyra Ardiani <desyardiani87@gmail.com>
2020-11-12 14:24:01 +0800

    TentangKami

--- src/views/Browse/TentangKami.jsx
+++ src/views/Browse/TentangKami.jsx
@@ -21,9 +21,10 @@ const TentangKami = (props) => {
           <Row>
             <Col lg="9">
               <h1 className="display-3 card-title">Tentang Kami</h1>
-              <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, perferendis fugiat totam nulla enim minus molestiae nisi possimus officia nam?</p>
+              <p className="lead">Apa itu From Home?</p>
               <hr className="my-2" />
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi magnam voluptatum nemo quidem ullam ex! Autem dolores nostrum recusandae. Repellendus.</p>
+              <p>From Home merupakan sebuah website yang kami bangun untuk para pencinta buku. Kami hadir untuk membantu anda menemukan "Your Next Favorite Book" 
+              </p>
             </Col>
             <Col lg="3">
             </Col>
